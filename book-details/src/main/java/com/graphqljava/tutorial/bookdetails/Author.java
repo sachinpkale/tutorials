8d0952d46dd0c22374c34dd03c3200115d461806
Sachin Kale <kalsac@amazon.com>
2021-02-05 09:56:06 +0530

    Add POJO for Book and Author

---
+++ book-details/src/main/java/com/graphqljava/tutorial/bookdetails/Author.java
@@ -0,0 +1,16 @@
+package com.graphqljava.tutorial.bookdetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class Author {
+    String id;
+    String firstName;
+    String lastName;
+}
--- book-details/src/main/java/com/graphqljava/tutorial/bookdetails/GraphQLDataFetchers.java
+++ book-details/src/main/java/com/graphqljava/tutorial/bookdetails/GraphQLDataFetchers.java
@@ -1,63 +1,45 @@
 package com.graphqljava.tutorial.bookdetails;
 
-import com.google.common.collect.ImmutableMap;
-import graphql.schema.DataFetcher;
-import graphql.schema.DataFetchingEnvironment;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import graphql.schema.DataFetcher;
 
 @Component
 public class GraphQLDataFetchers {
 
-    private static List<Map<String, String>> books = Arrays.asList(
-            ImmutableMap.of("id", "book-1",
-                    "name", "Harry Potter and the Philosopher's Stone",
-                    "pageCount", "223",
-                    "authorId", "author-1"),
-            ImmutableMap.of("id", "book-2",
-                    "name", "Moby Dick",
-                    "pageCount", "635",
-                    "authorId", "author-2"),
-            ImmutableMap.of("id", "book-3",
-                    "name", "Interview with the vampire",
-                    "pageCount", "371",
-                    "authorId", "author-3")
+    private static List<Book> books = Arrays.asList(
+            Book.builder().id("book-1").name("Harry Potter and the Philosopher's Stone").pageCount(223).author("author-1").build(),
+            Book.builder().id("book-2").name("Moby Dick").pageCount(635).author("author-2").build(),
+            Book.builder().id("book-3").name("Interview with the vampire").pageCount(371).author("author-3").build()
     );
 
-    private static List<Map<String, String>> authors = Arrays.asList(
-            ImmutableMap.of("id", "author-1",
-                            "firstName", "Joanne",
-                            "lastName", "Rowling"),
-            ImmutableMap.of("id", "author-2",
-                            "firstName", "Herman",
-                            "lastName", "Melville"),
-            ImmutableMap.of("id", "author-3",
-                            "firstName", "Anne",
-                            "lastName", "Rice")
+    private static List<Author> authors = Arrays.asList(
+            Author.builder().id("author-1").firstName("Joanne").lastName("Rowling").build(),
+            Author.builder().id("author-2").firstName("Herman").lastName("Melville").build(),
+            Author.builder().id("author-3").firstName("Anne").lastName("Rice").build()
     );
 
-    public DataFetcher<Map<String, String>> getBookByIdDataFetcher() {
+    public DataFetcher<Book> getBookByIdDataFetcher() {
         return dataFetchingEnvironment -> {
             String bookId = dataFetchingEnvironment.getArgument("id");
             return books
                     .stream()
-                    .filter(book -> book.get("id").equals(bookId))
+                    .filter(book -> book.getId().equals(bookId))
                     .findFirst()
                     .orElse(null);
         };
     }
 
-    public DataFetcher<Map<String, String>> getAuthorDataFetcher() {
+    public DataFetcher<Author> getAuthorDataFetcher() {
         return dataFetchingEnvironment -> {
-            Map<String,String> book = dataFetchingEnvironment.getSource();
-            String authorId = book.get("authorId");
+            Book book = dataFetchingEnvironment.getSource();
+            String authorId = book.getAuthor();
             return authors
                     .stream()
-                    .filter(author -> author.get("id").equals(authorId))
+                    .filter(author -> author.getId().equals(authorId))
                     .findFirst()
                     .orElse(null);
         };
