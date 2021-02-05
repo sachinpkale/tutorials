691ae33898846a8930674ec16374acbc1e9ad39f
Sachin Kale <kalsac@amazon.com>
2021-02-05 13:09:07 +0530

    Multiple requests caching and batching

--- book-details/src/main/java/com/graphqljava/tutorial/bookdetails/BookBatchLoader.java
+++ book-details/src/main/java/com/graphqljava/tutorial/bookdetails/BookBatchLoader.java
@@ -13,13 +13,27 @@ public class BookBatchLoader implements BatchLoader<String, Book> {
     private static List<Author> authors = Arrays.asList(
             Author.builder().id("author-1").firstName("Joanne").lastName("Rowling").build(),
             Author.builder().id("author-2").firstName("Herman").lastName("Melville").build(),
-            Author.builder().id("author-3").firstName("Anne").lastName("Rice").build()
+            Author.builder().id("author-3").firstName("Anne").lastName("Rice").build(),
+            Author.builder().id("author-4").firstName("Joanne").lastName("Rowling").build(),
+            Author.builder().id("author-5").firstName("Herman").lastName("Melville").build(),
+            Author.builder().id("author-6").firstName("Anne").lastName("Rice").build(),
+            Author.builder().id("author-7").firstName("Joanne").lastName("Rowling").build(),
+            Author.builder().id("author-8").firstName("Herman").lastName("Melville").build(),
+            Author.builder().id("author-9").firstName("Anne").lastName("Rice").build(),
+            Author.builder().id("author-10").firstName("Joanne").lastName("Rowling").build()
     );
 
     private static List<Book> books = Arrays.asList(
             Book.builder().id("book-1").name("Harry Potter and the Philosopher's Stone").pageCount(223).author(authors.get(0)).build(),
             Book.builder().id("book-2").name("Moby Dick").pageCount(635).author(authors.get(1)).build(),
-            Book.builder().id("book-3").name("Interview with the vampire").pageCount(371).author(authors.get(2)).build()
+            Book.builder().id("book-3").name("Interview with the vampire").pageCount(371).author(authors.get(2)).build(),
+            Book.builder().id("book-4").name("Book A").pageCount(478).author(authors.get(3)).build(),
+            Book.builder().id("book-5").name("Book B").pageCount(381).author(authors.get(4)).build(),
+            Book.builder().id("book-6").name("Book C").pageCount(666).author(authors.get(5)).build(),
+            Book.builder().id("book-7").name("Book D").pageCount(970).author(authors.get(6)).build(),
+            Book.builder().id("book-8").name("Book E").pageCount(1455).author(authors.get(7)).build(),
+            Book.builder().id("book-9").name("Book F").pageCount(822).author(authors.get(8)).build(),
+            Book.builder().id("book-10").name("Book G").pageCount(552).author(authors.get(9)).build()
     );
 
     @Override
--- book-details/src/main/java/com/graphqljava/tutorial/bookdetails/Main.java
+++ book-details/src/main/java/com/graphqljava/tutorial/bookdetails/Main.java
@@ -1,22 +1,23 @@
 package com.graphqljava.tutorial.bookdetails;
 
-import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.dataloader.DataLoader;
 import org.dataloader.DataLoaderOptions;
 import org.dataloader.DataLoaderRegistry;
 
-import com.google.common.collect.ImmutableMap;
-
 import graphql.ExecutionInput;
+import graphql.ExecutionResult;
 import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         GraphQLSchema schema = GraphQLProvider.buildSchema();
 
-        DataLoaderOptions dataLoaderOptions = DataLoaderOptions.newOptions().setCachingEnabled(false);
+        DataLoaderOptions dataLoaderOptions = DataLoaderOptions.newOptions().setCachingEnabled(true);
         DataLoader<String, Book> bookDataLoader = DataLoader.newDataLoader(new BookBatchLoader(), dataLoaderOptions);
 
         DataLoaderRegistry registry = new DataLoaderRegistry();
@@ -24,13 +25,27 @@ public class Main {
 
         GraphQL graphQL = GraphQL.newGraphQL(schema).build();
 
-        String query = "query { bookById(id: \"book-1\") { name author {firstName} } }";
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for(int i = 1; i < 11; i++) {
+            String query = "query { bookById(id: \"book-" + ((i % 2) + 1) + "\") { id name author {firstName} } }";
 
-        ExecutionInput executionInput = ExecutionInput.newExecutionInput()
-                                                      .query(query)
-                                                      .dataLoaderRegistry(registry)
-                                                      .build();
+            ExecutionInput executionInput = ExecutionInput.newExecutionInput()
+                                                          .query(query)
+                                                          .dataLoaderRegistry(registry)
+                                                          .build();
 
-        System.out.println(graphQL.execute(executionInput));
+            executorService.submit(() -> {
+                CompletableFuture<ExecutionResult> executeResult = graphQL.executeAsync(executionInput);
+                try {
+                    System.out.println((Object)executeResult.get().getData());
+                } catch (Exception e) {
+                    //Ignore for now
+                }
+            });
+        }
+
+        Thread.sleep(5000);
+
+        executorService.shutdown();
     }
 }
